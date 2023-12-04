f765fc6862521e491b1fa7cded44a75cf5eb02b6
Jörg Bayreuther <jb@visualjerk.de>
2023-12-04 22:41:43 +0100

    add db to stack

--- packages/collecting/stack/lib/collecting-stack.ts
+++ packages/collecting/stack/lib/collecting-stack.ts
@@ -1,6 +1,7 @@
 import { Stack, StackProps } from 'aws-cdk-lib'
 import * as route from 'aws-cdk-lib/aws-route53'
 import * as ec2 from 'aws-cdk-lib/aws-ec2'
+import * as rds from 'aws-cdk-lib/aws-rds'
 import * as cm from 'aws-cdk-lib/aws-certificatemanager'
 import { DockerImageAsset } from 'aws-cdk-lib/aws-ecr-assets'
 import * as ecs from 'aws-cdk-lib/aws-ecs'
@@ -42,6 +43,23 @@ export class CollectingStack extends Stack {
     const vpc = new ec2.Vpc(this, 'ArkhamHqCollectingVpc', { maxAzs: 1 })
     const cluster = new ecs.Cluster(this, 'ArkhamHqCollectingCluster', { vpc })
 
+    const db = new rds.DatabaseCluster(this, 'ArkhamHqCollectingDatabase', {
+      engine: rds.DatabaseClusterEngine.auroraPostgres({
+        version: rds.AuroraPostgresEngineVersion.VER_15_4,
+      }),
+      vpc,
+      writer: rds.ClusterInstance.provisioned('writer', {
+        publiclyAccessible: false,
+      }),
+    })
+    const dbHost = db.clusterEndpoint.hostname
+    const dbPort = db.clusterEndpoint.port
+    const dbUsername = db.secret!.secretValueFromJson('username').toString()
+    const dbPassword = db.secret!.secretValueFromJson('password').toString()
+
+    // DB connection string (e.g. "postgresql://postgres:password@0.0.0.0:5432/arkham-collecting")
+    const databaseUrl = `${db.engine?.engineType}://${dbUsername}:${dbPassword}@${dbHost}:${dbPort}/arkham-collecting`
+
     const dockerImage = new DockerImageAsset(
       this,
       'ArkhamHqCollectingUiImage',
@@ -67,6 +85,7 @@ export class CollectingStack extends Stack {
             AUTH_SERVICE_URL: props.authServiceUrl,
             BASE_URL: props.baseUrl,
             NEXT_TELEMETRY_DISABLED: '1',
+            DATABASE_URL: databaseUrl,
           },
         },
       }
