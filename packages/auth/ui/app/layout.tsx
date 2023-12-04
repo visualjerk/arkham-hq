b79c2b5029590fab6db345038ea8582701dbbf2c
Jörg Bayreuther <jb@visualjerk.de>
2023-12-04 16:07:42 +0100

    add bg image

--- packages/auth/ui/app/layout.tsx
+++ packages/auth/ui/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
       <html lang="en" className="bg-stone-100">
         <body className={inter.className}>
           <div className="md:grid md:grid-cols-2 xl:grid-cols-5 min-h-screen">
-            <div className="xl:col-span-3 bg-teal-800 p-5 md:p-10 lg:p-20 bg-gradient-to-b from-teal-700 to-teal-900">
+            <div className="xl:col-span-3 p-5 md:p-10 lg:p-20 bg-grunge from-teal-700 to-teal-900">
               <ArkhamHQLogo className="md:w-60 h-auto" />
             </div>
             <div className="xl:col-span-2 p-5 md:p-10 lg:p-20">{children}</div>
